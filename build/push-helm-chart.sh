2545eac8a1cf37661f3c7b3bfd6bafc1e8efb122
Morten Bork <mmb@kvalitetsit.dk>
2021-11-18 09:30:20 +0100

    Update naming

--- build/push-helm-chart.sh
+++ build/push-helm-chart.sh
@@ -8,8 +8,8 @@ git config --global user.name "Github Actions"
 git config --global user.email "development@kvalitetsit.dk"
 git clone https://github.com/KvalitetsIT/helm-repo
 cd helm-repo
-cp ../../service-*.tgz service
+cp ../../kitcaddy-*.tgz kitcaddy
 helm repo index . --url https://raw.githubusercontent.com/KvalitetsIT/helm-repo/master/
 git add -A
-git commit -m "Adding new service chart"
+git commit -m "Adding new kitcaddy chart"
 git push https://$1@github.com/KvalitetsIT/helm-repo
