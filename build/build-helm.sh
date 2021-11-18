df8f0b11a5cdb70e5edda7cf7c3017c8c936d248
Morten Bork <mmb@kvalitetsit.dk>
2021-11-18 09:43:09 +0100

    Update helm build

--- build/build-helm.sh
+++ build/build-helm.sh
@@ -1,2 +1,2 @@
-docker run -v /var/run/docker.sock:/var/run/docker.sock -v $(pwd):/src alpine/helm:3.2.3 package /src/$1 --app-version $2 --version $2 -d /src
+docker run -v /var/run/docker.sock:/var/run/docker.sock -v $(pwd):/src alpine/helm:3.2.3 package /src/helm/$1 --app-version $2 --version $2 -d /src
 
