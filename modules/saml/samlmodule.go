2d1f08ce32dc472f426671cacc53935d41ebb174
Sune Koch Hansen <sune@kvalitetsit.dk>
2022-01-27 14:11:38 +0100

    default httpOnly til true

--- modules/saml/samlmodule.go
+++ modules/saml/samlmodule.go
@@ -120,7 +120,13 @@ func (m *SamlProviderModule) Provision(ctx caddy.Context) error {
 	samlProviderConfig.ExternalUrl = m.ExternalUrl
 	samlProviderConfig.CookieDomain = m.CookieDomain
 	samlProviderConfig.CookiePath = m.CookiePath
-	samlProviderConfig.CookieHttpOnly, _ = strconv.ParseBool(m.CookieHttpOnly)
+	
+	defaultHttpOnly := true
+	if len(m.CookieHttpOnly) != 0 {
+		defaultHttpOnly, _ = strconv.ParseBool(m.CookieHttpOnly)
+	}
+	samlProviderConfig.CookieHttpOnly = defaultHttpOnly
+	
 	samlProviderConfig.AudienceRestriction = m.AudienceRestriction
 	samlProviderConfig.IdpMetaDataUrl = m.IdpMetaDataUrl
 	samlProviderConfig.SessionHeaderName = m.SessionHeaderName
