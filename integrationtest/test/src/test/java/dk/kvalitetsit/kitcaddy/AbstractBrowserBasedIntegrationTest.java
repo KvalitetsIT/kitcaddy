0ee718ae2201c6735fa99f3ec2abd63a7b015f86
fma-spec <fma@kvalitetsit.dk>
2026-08-06 16:35:08 +0200

    Attempt to update IntegrationTest Spring Boot 3.5.16

--- integrationtest/test/src/test/java/dk/kvalitetsit/kitcaddy/AbstractBrowserBasedIntegrationTest.java
+++ integrationtest/test/src/test/java/dk/kvalitetsit/kitcaddy/AbstractBrowserBasedIntegrationTest.java
@@ -1,14 +1,15 @@
 package dk.kvalitetsit.kitcaddy;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.remote.RemoteWebDriver;
 
 public class AbstractBrowserBasedIntegrationTest extends AbstractIntegrationTest {
 
 	public String doLoginFlow(RemoteWebDriver webdriver, String url, String username, String password) {
 		webdriver.get(url);
-		webdriver.findElementByName("username").sendKeys(username);
-		webdriver.findElementByName("password").sendKeys(password);
-		webdriver.findElementByName("login").click();
+		webdriver.findElement(By.name("username")).sendKeys(username);
+		webdriver.findElement(By.name("password")).sendKeys(password);
+		webdriver.findElement(By.name("login")).click();
 		String source = webdriver.getPageSource();
 		return source;
 	}
